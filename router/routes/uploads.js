4af70d06a5041756a46ad245ec5d0dce98d7661f
Stefan Hartmann <stefanhartmann@gmail.com>
2016-09-21 14:29:34 -0400

    fix uploads path

--- router/routes/uploads.js
+++ router/routes/uploads.js
@@ -2,12 +2,11 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require("body-parser");
 const multer = require('multer');
-const upload = multer({dest: '../../uploads/'});
+const upload = multer({dest: './uploads/'});
 const fs = require('fs');
-const formatter = require("../../formatter.js");
 const db = require('../../db/db');
 
-router.post('/', multer({ dest: '../../uploads/'}).single('upl'), function(req,res){
+router.post('/', multer({ dest: './uploads/'}).single('upl'), function(req,res){
 	console.log(req.file); //form files
   let filePath = req.file.path
 	res.status(204).end();
@@ -15,8 +14,8 @@ router.post('/', multer({ dest: '../../uploads/'}).single('upl'), function(req,r
     if (err) {
       res.send(err)
     } else {
-      var scriptArr = data.split("\n")
-      db.save_lines(scriptArr)
+      var scriptArr = data.split("\n");
+      db.save_lines(scriptArr);
     }
   })
 });
